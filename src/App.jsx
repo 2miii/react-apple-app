7c0fd63d12852b6dbe8a9f2fd3618dae31dab318
2miii <114133339+2miii@users.noreply.github.com>
2024-03-10 21:44:59 +0900

    container 구조

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 
+
 import './App.css'
 import Nav from './components/Nav'
 
 function App() {
 
   return (
-    <>
+    <Container>
     <Nav />
-     App
-    </>
+    </Container>
   )
 }
 
+// 컨테이너
+const Container = styled.main`
+postion: relative;
+display: block;
+top:70px;
+padding: 0 calc(3.vw + 5px);
+`
 export default App;
