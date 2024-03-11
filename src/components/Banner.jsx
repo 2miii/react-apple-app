1cecb83316043f1bdb60cfade8eacd98fb2e1ff9
2miii <114133339+2miii@users.noreply.github.com>
2024-03-11 22:05:03 +0900

    banner_movie_ifram

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,11 +4,13 @@ import axios from '../api/axios';
 import requests from '../api/requests';
 import{useState, useEffect } from 'react';
 import './Banner.css';
+import styled from 'styled-components';
 
 
 const Banner = () => {
 
   const [movie, setMovie] = useState(null);
+  const [isClicked, setIsClicked] = useState(false);
 
   //배너 이미지 가져오기
   useEffect(() => {
@@ -38,7 +40,7 @@ const Banner = () => {
 
   //영화설명 줄이기 - 특정 숫자에서 텍스트를 잘라줌
   const truncate = (string, number) => {
-    return string?.lngth > number ? string.string.substring(0,n) + "...": string;
+    return string?.length > number ? string.substring(0,number) + "...": string;
   }
 
   if (!movie) {
@@ -47,41 +49,89 @@ const Banner = () => {
             Loading...
         </div>
     )
-  } else {
+  }
+  
+  if (!isClicked){
     return (
-        <div
-          className='banner'
-          style={{
-            backgroundImage:`url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-            backgroundPosition:'top center',
-            backgroundsize:'cover'
-          }}
-        >
-         <div className='banner_contents'>
-            <h1 className='banner_title'>
-              {movie.title || movie.name || movie.original_title}
-            </h1>
-         </div>
-         <div>
-          {/* 해당 영화에 비디오가 있을 떄만 버튼 보여줌 */}
-            {movie.videos?.results[0]?.ket ?
+      <div
+        className="banner"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+          backgroundPosition: "top center",
+          backgroundSize: "cover",
+        }}
+      >
+
+        <div className="banner_contents">
+          <h1 className="banner_title">
+            {movie.title || movie.name || movie.original_title}
+          </h1>
+          <div>
+              {/* 해당 영화에 비디오가 있을 떄만 버튼 보여줌 */}
+            {movie.videos?.results[0]?.key ?
             <button
-               className='banner_button play'
+                className="banner_button play"
+                onClick={() => setIsClicked(true) }
             >
               play
             </button>
             :null
-          }
-         </div>
-         {/* 영화 줄거리요약 */}
-         <p className='banner_description'>
+            }
+          </div>
+            
+          {/* 영화 줄거리요약 */}
+          <p className="banner_description">
           {/* 100글자 이내로 줄임 */}
-          {truncate(movie.overview,100)}
-         </p>
+          {truncate(movie.overview, 100)}
+          </p>
         </div>
+        <div className="banner_fadeBottom"/>
+      </div>
     )
+  }else {
+      return(
+        <>
+          <Container>
+            <HomeContainer>
+            <Iframe src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?control=0&autoplay=1&mute=1`}></Iframe>
+            </HomeContainer>
+          </Container>
+          
+          <button 
+          className="video_button"
+          onClick={()=> setIsClicked(false)}
+          >
+            X
+          </button>
+        </>
+      )
   }
 }
 
+
+
+// iframe 비디오 설정
+const Iframe = styled.iframe`
+width:100%;
+height:100%;
+z-index:-1;
+opacity:0.65;
+border:none;
+
+`
+
+const Container = styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  flex-direction:column;
+  width:100%;
+  height:100vh;
+`
+
+const HomeContainer = styled.div`
+  width:100%;
+  height:100%;
+`
 
 export default Banner;
