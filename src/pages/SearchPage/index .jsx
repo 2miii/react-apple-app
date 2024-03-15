27c9fc70b33dce69603c52476fa2b5cede35bcaf
2miii <114133339+2miii@users.noreply.github.com>
2024-03-15 23:23:23 +0900

    search ui css

--- src/pages/SearchPage/index .jsx
+++ src/pages/SearchPage/index .jsx
@@ -1,12 +1,14 @@
 import {useEffect, useState} from "react";
-import {useLocation} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 import axios from "../../api/axios";
+import "./SearchPage.css";
 
 //searchTerm가져오기
 const SearchPage = () => {
   // const location = useLocation();
   // console.log(location);
   const [searchResults, setSearchResults] = useState([]);
+  const navigate = useNavigate();
 
   const useQuery = () => {
     return new URLSearchParams(useLocation().search)
@@ -30,7 +32,7 @@ const SearchPage = () => {
       const response = await axios.get(
         `/search/multi?include_adult=false&query=${searchTerm}`
       )
-      console.log(response);
+      // console.log(response); 가져오는지 확인
       setSearchResults(response.data.results);
 
     }catch (error){
@@ -38,10 +40,46 @@ const SearchPage = () => {
     }
   }
 
+  // 영화 이미지 가져오기
+  if(searchResults.length > 0) {
+    return (
+      <section className="search_container">
+        {searchResults.map((movie)=>{
+          if(movie.backdrop_path !== null && movie.media_type !== "person"){
+              const movieImageUrl = "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+              return(
+                <div className="movie" key={movie.id}>
+                  <div
+                    onClick={() => navigate(`/${movie.id}`)}
+                    className="movie_column-poster"
+                  >
+                    <img
+                       src={ movieImageUrl}
+                       alt="movie"
+                       className="movie_poster"
+                    />
+                  </div>
+                </div>
+              )
+          }
+
+
+        })}
+      </section>
+    )
+  }else {
+    return(
+      <section className="no_results">
+        <div className="no_results-text">
+          <p>
+            검색하신 '{searchTerm}'에 맞는 영화가 없습니다.
+          </p>
+        </div>
+      </section>
+    )
+  }
   
-  return (
-    <div>SearchPage</div>
-  )
+  
 }
 
 export default SearchPage;
